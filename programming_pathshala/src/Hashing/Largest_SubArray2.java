f310539dfb3fc82fa3958215f802f8c07cfde851
ACER <ACER@Sourav>
2022-04-03 22:02:12 +0530

    Largest SubArray 2

--- programming_pathshala/src/Hashing/Largest_SubArray2.java
+++ programming_pathshala/src/Hashing/Largest_SubArray2.java
@@ -1,35 +1,16 @@
 package Hashing;
 
 import java.util.*;
-public class Largest_SubArray
+public class Largest_SubArray2
 {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		
+		int k = sc.nextInt();
 		int n = sc.nextInt();
 		int arr[] = new int[n];
 		for(int i = 0; i < n; i++)
 			arr[i] = sc.nextInt();
-//		
-//		for(int i = 1; i < n; i++)
-//			arr[i] += arr[i - 1];
-//		
-//		Map<Integer, Integer> map = new HashMap<>();
-//		map.put(0, -1);
-//		int ans = Integer.MIN_VALUE;
-//		for(int i = 0; i < n; i++)
-//		{
-//			if(!map.containsKey(arr[i]))
-//			{
-//				map.put(arr[i], i);
-//			}
-//			if(map.containsKey(arr[i]))
-//			{
-//				ans = max(ans, i - map.get(arr[i]));
-//			}
-//				
-//		}
 		
 		int ans = 0;
 		Map<Integer, Integer> map = new HashMap<>();
@@ -39,11 +20,11 @@ public class Largest_SubArray
 		for(int i = 0; i < n; i++)
 		{
 			psum+= arr[i];
+			if(map.containsKey(psum - k))
+				ans = max(ans, i - map.get(psum - k));
 			if(!map.containsKey(psum))
-				map.put(psum, i);
-			else
 			{
-				ans = max(ans, i - map.get(psum));
+				map.put(psum, i);
 			}
 		}
 		
